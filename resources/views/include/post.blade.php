6abf7952ee78725f89a09fd2723856002833e421
Ezmiron Deniku <ezmiron.deniku@studio.unibo.it>
2023-03-08 14:45:45 +0100

    add new views for profile

---
+++ resources/views/include/post.blade.php
@@ -0,0 +1,16 @@
+<!-- component -->
+<div class="relative max-w-md mx-auto md:max-w-2xl mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl">
+    <div class="px-6 pt-6">
+        <div class="flex flex-wrap justify-center mt-26 bg-white">
+            <div class="flex justify-center w-full">
+                <h1>{{ $user->name }}</h1>
+            </div>
+            <div class="bg-black h-24 w-24">
+
+            </div>
+            <div class="bg-yellow-200 h-24 w-24">
+
+            </div>
+        </div>
+    </div>
+</div>
